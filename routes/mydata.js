7b06de7b63b707fc2f2fd7174e73626f0d26b47c
ramuunnava <ramuunnava1@gmail.com>
2022-10-24 19:20:38 -0500

    added mydata filee

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'unnava ramu' });
+  res.render('mydata', { title: 'Unnava Ramu' });
 });
 
 module.exports = router;
